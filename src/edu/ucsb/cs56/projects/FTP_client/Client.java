8d2538898513911dc00447e197867443f7db1119
Administrator <wenjie@ubuntu.(none)>
2013-06-03 00:15:44 -0700

    implement downlaod function. Download files into current local directory

--- src/edu/ucsb/cs56/projects/FTP_client/Client.java
+++ src/edu/ucsb/cs56/projects/FTP_client/Client.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs56.projects.FTP_client;
 
 import java.util.*;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,13 +14,33 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPConnectionClosedException;
 public class Client {
 	private FTPClient client;
 	
 	public Client()	{
 		client = new FTPClient();
 	}
-	public void go ()	{
+	public void download()	{
+		System.out.println("input file to download:");
+		Scanner sc = new Scanner (System.in);
+		String input = sc.nextLine();
+		String[] filenames = input.split("/");
+		String filename = filenames[filenames.length-1];
+		try
+		{
+			File file = new File(filename);
+			OutputStream outputstream = new FileOutputStream(file);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			client.retrieveFile(input, outputstream);			
+		}
+		catch (FTPConnectionClosedException e){ }
+		catch (IOException e){	}
+		
+	}
+	public void connect ()	{
 		System.out.println("input host name:");
 		Scanner sc = new Scanner (System.in);
 		String host = sc.nextLine();
@@ -42,14 +63,14 @@ public class Client {
 			}
 		catch(IOException e) {
 			System.out.println("Connection fail.");
-		} 
-		
+		}
 	}
 	public static void main (String args[])	{
 		
 		System.out.println("Client start!");
 		Client newClient = new Client();
-		newClient.go();
+		newClient.connect();
+		newClient.download();
 	}
 	
 }
